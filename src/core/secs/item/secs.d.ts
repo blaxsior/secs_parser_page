6bee38777224e1c3770975f851255a9b978b81ac
blaxsior <heejun2798@gmail.com>
2025-01-28 13:13:12 +0900

    chore: Secs2 Item 타입 일부 수정(F1 F2 Jis-8 제거), SecsItem 타입에 대한 sml / format 작성

--- src/core/secs/item/secs.d.ts
+++ src/core/secs/item/secs.d.ts
@@ -1,36 +1,40 @@
 // // https://www.peergroup.com/resources/secs-message-language/
 // // SML info
 
-export type Secs2ItemSML = 'L' | 'B' | 'BOOLEAN' | 'A' | 'J' |
+export type Secs2ItemSML = 'L' | 'B' | 'BOOLEAN' | 'A' | 
+    // 'J' |
     'I8' | 'I1' | 'I2' | 'I4' |
-    'F8' | 'F1' | 'F2' | 'F4' |
+    'F8' | 'F4' |
     'U8' | 'U1' | 'U2' | 'U4';
 
 type Secs2ItemMap<T extends Secs2ItemSML> =
-    T extends 'L' ? Secs2Item[] :  // List
+    T extends 'L' ? Secs2Item[] :
     T extends 'B' ? Uint8Array :
     T extends 'BOOLEAN' ? boolean[] :
     T extends 'A' ? string :
     T extends
-    'J' | 'I8' | 'I1' | 'I2' | 'I4' |
-    'F8' | 'F1' | 'F2' | 'F4' |
+    // 'J' | 
+    'I8' | 'I1' | 'I2' | 'I4' |
+    'F8' | 'F4' |
     'U8' | 'U1' | 'U2' | 'U4' ? number[] :
     never;
 /**
  * SECS-II의 각 아이템 타입
  */
-export type Secs2ItemType<T> = {
+export type Secs2ItemType<T extends Secs2ItemSML> = {
     sml: T,
-    format: string,
     formatCode: number
 };
-// Secs2ItemMap[T];
+
+export type Secs2ItemInfo = {
+    [K in Secs2ItemSML]: Secs2ItemType<K>
+};
 
 export type Secs2Item = {
     [K in Secs2ItemSML]: {
-        type: K;
+        type: Secs2ItemInfo[K];
         name?: string;
         length: number;
-        items: Secs2ItemMap<K>;
+        item: Secs2ItemMap<K>;
     };
 }[Secs2ItemSML];
--- src/core/secs/item/secs_item.ts
+++ src/core/secs/item/secs_item.ts
@@ -1,2 +1,22 @@
-import { Secs2Item } from "./secs";
+import type { Secs2ItemInfo } from "./secs";
 
+const secsItemInfo: Secs2ItemInfo = {
+    L: { sml: 'L', formatCode: 0o00 },
+    B: { sml: 'B', formatCode: 0o10 },
+    BOOLEAN: { sml: 'BOOLEAN', formatCode: 0o11 },
+    A: { sml: 'A', formatCode: 0o20 },
+    I8: { sml: 'I8', formatCode: 0o30 },
+    I1: { sml: 'I1', formatCode: 0o31 },
+    I2: { sml: 'I2', formatCode: 0o32 },
+    I4: { sml: 'I4', formatCode: 0o34 },
+    F8: { sml: 'F8', formatCode: 0o40 },
+    F4: { sml: 'F4', formatCode: 0o44 },
+    U8: { sml: 'U8', formatCode: 0o50 },
+    U1: { sml: 'U1', formatCode: 0o51 },
+    U2: { sml: 'U2', formatCode: 0o52 },
+    U4: { sml: 'U4', formatCode: 0o54 },
+} as const;
+
+export {
+    secsItemInfo
+};
