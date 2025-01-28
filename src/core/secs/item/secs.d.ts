e6b6830edefcb2b503c6dce9bf11436b716e4fb1
blaxsior <heejun2798@gmail.com>
2025-01-28 12:32:53 +0900

    chore: Secs2 Item에 대한 타입 정의

--- src/core/secs/item/secs.d.ts
+++ src/core/secs/item/secs.d.ts
@@ -6,26 +6,16 @@ export type Secs2ItemSML = 'L' | 'B' | 'BOOLEAN' | 'A' | 'J' |
     'F8' | 'F1' | 'F2' | 'F4' |
     'U8' | 'U1' | 'U2' | 'U4';
 
-type Secs2ItemMap = {
-    L: Secs2Item[];
-    B: Uint8Array;
-    BOOLEAN: boolean[];
-    A: string;
-    J: number;
-    I8: number[];
-    I1: number[];
-    I2: number[];
-    I4: number[];
-    F8: number[];
-    F1: number[];
-    F2: number[];
-    F4: number[];
-    U8: number[];
-    U1: number[];
-    U2: number[];
-    U4: number[];
-};
-
+type Secs2ItemMap<T extends Secs2ItemSML> =
+    T extends 'L' ? Secs2Item[] :  // List
+    T extends 'B' ? Uint8Array :
+    T extends 'BOOLEAN' ? boolean[] :
+    T extends 'A' ? string :
+    T extends
+    'J' | 'I8' | 'I1' | 'I2' | 'I4' |
+    'F8' | 'F1' | 'F2' | 'F4' |
+    'U8' | 'U1' | 'U2' | 'U4' ? number[] :
+    never;
 /**
  * SECS-II의 각 아이템 타입
  */
@@ -36,11 +26,11 @@ export type Secs2ItemType<T> = {
 };
 // Secs2ItemMap[T];
 
-export type Secs2Item =
-    { type: 'L', name?: string, items: Secs2Item[] } |
-    { type: 'B', name?: string, items: number[] } |
-    { type: 'BOOLEAN', name?: string, items: boolean[] } |
-    { type: 'A', name?: string, items: string } |
-    { type: 'I8' | 'I1' | 'I2' | 'I4' |
-    'F8' | 'F1' | 'F2' | 'F4' |
-    'U8' | 'U1' | 'U2' | 'U4', name?: string, items: number[] };
+export type Secs2Item = {
+    [K in Secs2ItemSML]: {
+        type: K;
+        name?: string;
+        length: number;
+        items: Secs2ItemMap<K>;
+    };
+}[Secs2ItemSML];
--- src/core/secs/item/secs_item.ts
+++ src/core/secs/item/secs_item.ts
@@ -1,12 +1,2 @@
 import { Secs2Item } from "./secs";
 
-
-const item: Secs2Item = {
-    type: 'L',
-    items: [
-        {
-            type: 'A',
-            items: ['str']
-        }
-    ]
-};
