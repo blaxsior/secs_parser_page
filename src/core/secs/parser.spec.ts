6d8c7b5d867f749aa70e25c07a2b62239bfe3b7f
blaxsior <heejun2798@gmail.com>
2025-01-28 15:48:22 +0900

    feat: secs-II 메시지 format code, byteLength, byte 파싱 기능 구현

--- src/core/secs/parser.spec.ts
+++ src/core/secs/parser.spec.ts
@@ -1,16 +1,79 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import { Secs2MessageParser } from './parser';
-import { Secs2Item } from './item/secs';
+import { Secs2ItemInfo } from './item/secs';
+import { secsInfoMap } from './item/secs_item_info';
 
 describe('SecsParser Test', () => {
-    it("should parse SecsItem To binary byte array", (c) => {
-        const parser = new Secs2MessageParser();
+    let parser: Secs2MessageParser;
 
-        const item: Secs2Item = {
-        };
-        
-        const result = parser.parse(item);
-        
-        expect(result);
+    beforeEach(() => {
+        parser = new Secs2MessageParser(secsInfoMap);
+    });
+
+    describe('parseType', () => {
+        it("포맷 코드를 파싱, 대응되는 타입 획득", () => {
+            const data = 0b000000_01; // 길이가 1인 list
+            const expected: Secs2ItemInfo<'L'> = { sml: 'L', formatCode: 0o00 };
+
+            const result: Secs2ItemInfo = parser.parseType(data);
+
+            expect(result).toEqual(expected);
+        });
+
+        it("매칭되지 않는 format code인 경우 error 발생", () => {
+            const data = 0b111111_01; // 존재하지 않는 format
+
+            expect(() => {
+                parser.parseType(data);
+            }).toThrow();
+        });
+    });
+
+    describe('parseByteLength', () => {
+        it("byte 길이를 파싱해 반환", () => {
+            const data = 0b000000_10; // byte 길이 = 2
+            const expected = 2;
+
+            const result: number = parser.parseByteLength(data);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('parseLength', () => {
+        it('count만큼 byte를 읽어 길이 획득', () => {
+            const data = new Uint8Array([0xAB, 0xCD, 0xEF]);
+            const idx = 1; // 인덱스 1부터
+            const count = 2; // 2byte 읽기
+
+            const expected = 52719; // 0xCDEF
+
+            const result = parser.parseLength(data, idx, count);
+            expect(result).toEqual(expected);
+        });
+
+        it('읽는 byte 개수 = count가 [0..3]을 벗어나면 예외 발생', () => {
+            const data = new Uint8Array([0xAB, 0xCD, 0xEF]);
+            const idx = 1; // 인덱스 1부터
+
+            const wrong_count1 = -1;
+            expect(() => {
+                parser.parseLength(data, idx, wrong_count1);
+            }).toThrowError('count must be in');
+
+            const wrong_count2 = 4;
+            expect(() => {
+                parser.parseLength(data, idx, wrong_count2);
+            }).toThrowError('count must be in');
+        });
+
+        it('idx 위치부터 count 개의 byte를 읽을 수 없다면 예외 발생', () => {
+            const data = new Uint8Array([0xAB, 0xCD, 0xEF]);
+            const idx = 1; // 인덱스 1부터
+            const wrong_count1 = 3; // 3개
+
+            expect(() => {
+                parser.parseLength(data, idx, wrong_count1);
+            }).toThrowError('cannot read');
+        });
     });
 });
--- src/core/secs/parser.ts
+++ src/core/secs/parser.ts
@@ -1,10 +1,42 @@
-import type { Secs2Item } from "./item/secs";
+import type { Secs2Item, Secs2ItemInfo } from "./item/secs";
+import { Secs2ItemInfoMap } from "./item/secs_item_info";
 
 /**
  * @description secs-II 메시지를 파싱 & 언파싱하기 위한 클래스
  */
 export class Secs2MessageParser {
-    parse(item: Secs2Item): Int8Array {
+    parseLength(buffer: Uint8Array, idx: number, count: number): number {
+        if(count < 0 || count > 3) throw new Error(`count must be in [0..3], but count is ${count}`);
+        if((idx + count) > buffer.length) throw new Error(`cannot read data at ${idx + count}, buffer length is ${buffer.length}`);
+
+        let length = 0;
+
+        for(let i = idx; i < idx + count; i++) {
+            length <<= 8; // byte 밀기
+            length |= buffer[i]; // 하위 byte 작성
+        }
+
+        return length;
+    }
+    parseByteLength(data: number): number {
+        const byteLengthMask = 0b000000_11;
+        return data & byteLengthMask;
+    }
+    private itemMap: Secs2ItemInfoMap;
+
+    constructor(itemMap: Secs2ItemInfoMap) {
+        this.itemMap = itemMap;
+    }
+
+    parseType(byte: number): Secs2ItemInfo {
+        // 상위 6bit
+        const formatCodeMask = 0b111111_00;
+        const formatCode = formatCodeMask & byte;
+
+        return this.itemMap.fromFormatCode(formatCode);
+    }
+
+    parse(data: Int8Array): Secs2Item {
         throw new Error('Method not implemented.');
     }
 }
