2fbaab9ca01dcc858d3f146708d56e8d6550f282
blaxsior <heejun2798@gmail.com>
2025-02-03 10:53:11 +0900

    chore: test를 위한 설정 추가

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+import viteConfig from './vite.config';
+import { defineConfig, mergeConfig } from "vitest/config";
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            environment: "happy-dom"
+        }
+    })
+);
