7da825871e48b02be6f65fa022a629e66d1b1902
blaxsior <heejun2798@gmail.com>
2025-02-26 15:24:21 +0900

    fix: react-router이 상대 경로 인식 못하는 문제 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import './index.css'
+const basename = import.meta.env.MODE === "production" ? "/secs_parser_page" : "/";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
