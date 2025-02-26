6f29b9be7566cf6c37c081ae6c12cec148a6a68e
blaxsior <heejun2798@gmail.com>
2025-02-26 16:25:16 +0900

    chore: /secs_parser_page 경로를 명시하던 설정을 원래대로 복구

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-const basename = import.meta.env.MODE === "production" ? "/secs_parser_page" : "/";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
